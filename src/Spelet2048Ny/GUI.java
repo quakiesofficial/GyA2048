d7578c681d43d5eb7781da931c01f0d235ebaac8
SexyKitten <marcus.soderlund@rosendalsgymnasiet.se>
2023-03-14 12:53:23 +0100

    Scorefile

--- src/Spelet2048Ny/GUI.java
+++ src/Spelet2048Ny/GUI.java
@@ -1,6 +1,7 @@
 package Spelet2048Ny;
 
 import javax.imageio.ImageIO;
+import javax.management.StringValueExp;
 import javax.swing.*;
 import javax.swing.border.LineBorder;
 import java.awt.*;
@@ -180,40 +181,6 @@ public class GUI {
             System.out.println("Fel: " + e.getMessage());
         }
     }
-    private void scorefileUpdate() {
-        try {
-            FileWriter fileWriterRandom = new FileWriter("files/scorefiler_random");
-            FileWriter fileWriterCorner = new FileWriter("files/scorefiler_corner",true);
-            FileWriter fileWriterAlgorithm = new FileWriter("files/scorefiler_algorithm",true);
-            PrintWriter printRandom = new PrintWriter(fileWriterRandom);
-            FileReader fileReaderRandom = new FileReader("files/scorefiler_random");
-            BufferedReader bufferedReaderRandom = new BufferedReader(fileReaderRandom);
-
-            String numberOfRuns = bufferedReaderRandom.readLine();
-            int numbrOfRunsint = Integer.parseInt(numberOfRuns);
-            String totalScore = bufferedReaderRandom.readLine();
-            int totalScoreint = Integer.parseInt(totalScore);
-            totalScore += board.getScore();
-            System.out.println(numberOfRuns);
-            System.out.println(numbrOfRunsint);
-            System.out.println(totalScore);
-            System.out.println(totalScoreint);
-
-            //int avrageScore = totalScore/numberOfRuns;
-
-            printRandom.println(numbrOfRunsint + "");
-            //printRandom.println(totalScore);
-            //printRandom.println(avrageScore);
-
-
-            fileWriterRandom.close();
-            fileWriterCorner.close();
-            fileWriterAlgorithm.close();
-        } catch (IOException e) {
-            System.out.println("fel");
-        }
-    }
-
 
     public void lostDialog() {
         JDialog lostDialog = new JDialog(frame);
