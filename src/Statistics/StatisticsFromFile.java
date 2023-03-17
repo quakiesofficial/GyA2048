ed391793e6fe2d2c0fa61c98f694668501b8f621
quakiesofficial <niklas.sundin@rosendalsgymnasiet.se>
2023-03-17 18:47:09 +0100

    lade till max score i tabellen

--- src/Statistics/StatisticsFromFile.java
+++ src/Statistics/StatisticsFromFile.java
@@ -21,7 +21,8 @@ public class StatisticsFromFile {
     private int highestTile = 0;
     private ArrayList<Integer> allHighestTiles;
     private int mostFrequentTile = 0;
-    private int frequency;
+    private int counter;
+    private int highestScore;
 
 
     public StatisticsFromFile() {
@@ -64,6 +65,7 @@ public class StatisticsFromFile {
         amountOfColumns = 0;
         depth = 0;
         mostFrequentTile = 0;
+        highestScore = 0;
         try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(getDirectory()));
             row = bufferedReader.readLine();
@@ -72,6 +74,8 @@ public class StatisticsFromFile {
                 amountOfColumns = rowSplit.length;
                 if (Integer.parseInt(rowSplit[1]) > highestTile )
                     highestTile = Integer.parseInt(rowSplit[1]);
+                if (Integer.parseInt(rowSplit[0]) > highestScore)
+                    highestScore = Integer.parseInt(rowSplit[0]);
                 averageScore += Double.parseDouble(rowSplit[0]);
                 allHighestTiles.add(Integer.parseInt(rowSplit[1]));
                 depth = Integer.parseInt(rowSplit[rowSplit.length-1]);
@@ -81,18 +85,17 @@ public class StatisticsFromFile {
         } catch (IOException e) {
             System.out.println("Fel: " + e.getMessage());
         }
-        int counter = 0;
-        System.out.println(allHighestTiles);
+        counter = 0;
         try {
             for (int i = 0; i < allHighestTiles.size(); i++) {
 
-                frequency = Collections.frequency(allHighestTiles, allHighestTiles.get(i));
+                int frequency = Collections.frequency(allHighestTiles, allHighestTiles.get(i));
                 if (frequency == counter && allHighestTiles.get(i) > mostFrequentTile) {
                     mostFrequentTile = allHighestTiles.get(i);
                 }
                 else if (frequency > counter) {
-                    mostFrequentTile = allHighestTiles.get(i);
                     counter = frequency;
+                    mostFrequentTile = allHighestTiles.get(i);
                 }
             }
         } catch (NullPointerException e) {
@@ -106,13 +109,15 @@ public class StatisticsFromFile {
     }
     private void setInformationInArray() {
         try {
-            tableData = new String[amountOfColumns+1][2];
+            tableData = new String[4][2];
             tableData[0][0] = "Average score";
             tableData[0][1] = String.valueOf(averageScore);
             tableData[1][0] = "Highest tile";
             tableData[1][1] = String.valueOf(highestTile);
             tableData[2][0] = "Mode value";
-            tableData[2][1] = String.valueOf(mostFrequentTile + " (" + frequency + ")");
+            tableData[2][1] = String.valueOf(mostFrequentTile + " (" + counter + ")");
+            tableData[3][0] = "Highest Score";
+            tableData[3][1] = String.valueOf(highestScore);
         } catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Filen är tom: " + e.getMessage());
         }
